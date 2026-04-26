3526cbade68739f634385ba813497f779b715b2b
Julia <juliaverascoelho53@gmail.com>
2026-04-26 16:58:53 -0300

    Remove commented-out code and clean up app.js

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados);
         return numeroEscolhido;
     }
 }
